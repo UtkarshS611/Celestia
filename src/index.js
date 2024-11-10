5243d91248d82413d8a81f0d4261bfd7af7a7a89
Utkarsh Singh <utkarsh.singh6113@gmail.com>
2024-11-10 12:40:43 +0530

    landing page initialized

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Home from '../src/Components/Home/Home';
+import Footer from '../src/Components/Footer/Footer';
+import Sidebar from '../src/Components/Sidebar/Sidebar';
+
+export {
+    Home,
+    Sidebar,
+    Footer,
+}
